bfc02bf0f21b2f431dfdb39da90cd4fd33f77dbd
olegpan1 <olegpan@hotmail.com>
2021-03-26 08:33:56 +0700

    9.1. String [#504581]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -3,7 +3,7 @@ package ru.job4j.calculator;
 public class ArgMethod {
 
     public static void hello(String name, int age) {
-        System.out.println("Hello, " + name + ",  " + age+ " years old.");
+        System.out.println("Hello, " + name + ",  " + age + " years old.");
     }
 
     public static void main(String[] args) {
