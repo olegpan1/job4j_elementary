e5440d8c8906bc424c323afc9bbe28f7dbbd7877
olegpan1 <olegpan@hotmail.com>
2021-03-27 19:04:59 +0700

    4.5. Отладка программы в IDEA [#504517]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,14 +1,15 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, double k) {
-        double high = p / (2 * (k + 1));
-        double length = high * k;
-        return high * length;
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(6, 2);
+        int result1 = SqArea.square(6, 2);
         System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
     }
 }
--- test/ru/job4j/condition/SqAreaTest.java
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -1,35 +1,27 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 
 public class SqAreaTest {
 
     @Test
-    public void whenP6K2Square2() {
-        int expected = 2;
+    public void whenp4k1s1() {
+        int k = 1;
+        int p = 4;
+        int expected = 1;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
+
+    @Test
+    public void whenp6k2s2() {
+        int k = 2;
         int p = 6;
-        double k = 2;
-        double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
+        int expected = 2;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
     }
-
-    @Test
-    public void whenP8K4Square256() {
-        double expected = 2.56;
-        int p = 8;
-        double k = 4;
-        double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void whenP48K7Square63() {
-        double expected = 63;
-        int p = 48;
-        double k = 7;
-        double out = SqArea.square(p, k);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
 }
