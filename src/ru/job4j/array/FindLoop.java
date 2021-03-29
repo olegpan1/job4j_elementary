dca09e82fa56eb754e9b501183f363ee1e881dd8
olegpan1 <olegpan@hotmail.com>
2021-03-29 18:14:41 +0700

    6.5. Поиск индекса в диапазоне. [#504545]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -1,9 +1,9 @@
 package ru.job4j.array;
 
 public class FindLoop {
-    public static int indexOf(int[] data, int el) {
+    public static int indexOf(int[] data, int el, int start, int finish) {
         int rsl = -1;
-        for (int i = 0; i < data.length; i++) {
+        for (int i = start; i <= finish; i++) {
             if (data[i] == el) {
                 rsl = i;
                 break;
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -5,30 +5,24 @@ import org.junit.Test;
 
 public class FindLoopTest {
     @Test
-    public void whenArrayHas5Then0() {
-        int[] data = new int[]{5, 3, 23, 7};
-        int el = 5;
-        int rsl = FindLoop.indexOf(data, el);
-        int expected = 0;
-        Assert.assertEquals(expected, rsl);
-    }
-
-    @Test
-    public void whenArrayHas2ThenNone() {
-        int[] data = new int[]{5, 3, 23, 7};
-        int el = 2;
-        int rsl = FindLoop.indexOf(data, el);
+    public void whenNotFind() {
+        int[] data = new int[]{5, 2, 10, 2, 4};
+        int el = 9;
+        int start = 1;
+        int finish = 3;
+        int result = FindLoop.indexOf(data, el, start, finish);
         int expected = -1;
-        Assert.assertEquals(expected, rsl);
+        Assert.assertEquals(expected, result);
     }
 
     @Test
-    public void whenArrayHasMinus1Then2() {
-        int[] data = new int[]{0, 4, -1, 5, 3, -1, 23, 7};
-        int el = -1;
-        int rsl = FindLoop.indexOf(data, el);
-        int expected = 2;
-        Assert.assertEquals(expected, rsl);
+    public void whenFind3() {
+        int[] data = new int[]{5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
     }
-
 }
